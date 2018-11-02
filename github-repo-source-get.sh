b4e283f0d6a17c10da30b97e2ef835df0ceaad08
Akai <akai-z@users.noreply.github.com>
2018-11-02 22:02:32 +0200

    Add global variables

--- github-repo-source-get.sh
+++ github-repo-source-get.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 set -Eeuo pipefail
+
+readonly REPO_BASE_URL="https://api.github.com/repos/%s/%s"
+readonly RELEASE_URL="${REPO_BASE_URL}/releases/%s"
+readonly TARBALL_URL="${REPO_BASE_URL}/tarball/%s"
+readonly DEFAULT_GIT_REF="latest"
+readonly SOURCE_TARBALL="github-repo-source.tar.gz"
+readonly REQUIRED_DEPS=("curl" "tar" "gzip")
+
+GITHUB_USERNAME=""
+GITHUB_REPO=""
+GIT_REF=$DEFAULT_GIT_REF
+SOURCE_PATH=""
